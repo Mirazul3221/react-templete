f2543b249f92b69367680de759ea63f943ae8b0a
Mirazul Islam <mirazul3221@gmail.com>
2024-01-09 19:12:01 +0600

    U

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,8 +2,73 @@ import React from 'react'
 
 const Footer = () => {
   return (
-      <div>
-          
+      <div className='flex justify-between py-10 bg-gray-200 grid grid-cols-5'>
+          <div className="item">
+              <ul>
+                  <li className='px-4'><a href="/"><img src="./images/logo.png" alt="" /></a></li>
+              </ul>
+              <p className='px-4 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ab maiores dolorum nulla possimus cumque natus, culpa dicta obcaecati vel perspiciatis, repudiandae quae repellat beatae sint ipsam explicabo laudantium similique?</p>
+          </div>
+          <div className="item px-5 ">
+              <div className="">
+                  <h2 className='text-2xl font-medium'> Our Services</h2>
+                  <div className="bg-gray-700 w-20 h-[3px] rounded-r-full"></div>
+                  <ul className='mt-6'>
+                      <li className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Replacement</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Orthopaedics</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Pain</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Physical Therapy</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Diagnostic Imaging</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Sports Medicine</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Hand Pain</a></li>
+                  </ul>
+              </div>
+              
+          </div>
+          <div className="item px-5 ">
+              <div className="">
+                  <h2 className='text-2xl font-medium'>Patient Resources</h2>
+                  <div className="bg-gray-700 w-20 h-[3px] rounded-r-full"></div>
+                  <ul className='mt-6'>
+                      <li className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Replacement</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Orthopaedics</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Pain</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Physical Therapy</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Diagnostic Imaging</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Sports Medicine</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Hand Pain</a></li>
+                  </ul>
+              </div>
+              
+          </div>
+          <div className="item px-5 ">
+              <div className="">
+                  <h2 className='text-2xl font-medium'> Shoulder Pain</h2>
+                  <div className="bg-gray-700 w-20 h-[3px] rounded-r-full"></div>
+                  <ul className='mt-6'>
+                      <li className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Replacement</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Orthopaedics</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Shoulder Pain</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Physical Therapy</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Diagnostic Imaging</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Sports Medicine</a></li>
+                      <li  className='hover:underline duration-500 text-gray-600 hover:text-black'><a className='' href="/">Hand Pain</a></li>
+                  </ul>
+              </div>
+              
+          </div>
+          <div className="item px-5 ">
+              <div className="">
+                  <h2 className='text-2xl font-medium'>Contact Us</h2>
+                  <div className="bg-gray-700 w-20 h-[3px] rounded-r-full"></div>
+                  <h2>phone number: 65334534653</h2>
+                  <p>Address line 1, Address line 2, Hamilton, NY 08691</p>
+                  <p>Language Assistance Available in:
+Español, 中文, 한국어, Português, ગુજરાતી, Polskie,
+Italiano, العربية, Việt, Français, اُردُو</p>
+              </div>
+              
+          </div>
     </div>
   )
 }
